1ee069be6325786ea2f95d1cbcb7f0b4e90e6395
Thanakorn Kitviriya <dukethanakorn@gmail.com>
2025-03-26 08:42:38 +0700

    Update version to 2.2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5001 // Updated to your port
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('2.1') // Changed from '2' to '2.1'
+  res.send('2.2') // Changed to '2.2'
 })
 
 app.get('/health', (req, res) => {
